1538b8cb8f51935a2e3137b2da3f83bc11a4eafc
David Carboni <david@carboni.io>
2022-03-01 21:29:41 +0000

    Draft of ApplicationLoadBalancedFargateService

--- infrastrusture/eleos/eleos_stack.py
+++ infrastrusture/eleos/eleos_stack.py
@@ -1,19 +1,34 @@
 from aws_cdk import (
     # Duration,
+    RemovalPolicy,
     Stack,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+from aws_cdk import aws_ecs_patterns
+from aws_cdk import aws_ecs
+from aws_cdk import aws_ecr
+
 
 class EleosStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        ecr_repository = aws_ecr.Repository(
+            self, "EcrRepository",
+            repository_name="eleos",
+            removal_policy=RemovalPolicy.DESTROY)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EleosQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        task_image_options = aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+            image=aws_ecs.ContainerImage.from_registry(
+                name=ecr_repository.repository_name),
+            container_port=8069)
+
+        aws_ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, "FargateService",
+            assign_public_ip=True,
+            desired_count=2,
+            memory_limit_mib=1024,
+            cpu=512,
+            task_image_options=task_image_options)
