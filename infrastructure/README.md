1b0a956c0622b07febd75347468c1a96fde61899
4-3is4-me <timwornell@hotmail.com>
2022-08-25 13:00:57 +0100

    Added tests and update README

--- infrastructure/README.md
+++ infrastructure/README.md
@@ -5,22 +5,44 @@ This infrastructure is built with the AWS Cloud Development Kit (CDK).
 
 ## Initial setup
 
+ * Full setup details can be found at [AWS CDK Getting Started](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html)
+ * A getting started tutorial from AWS can be found [here](https://aws.amazon.com/getting-started/guides/setup-cdk/)
+
 You will need the following setup:
 
  * Node.js and NPM are required to generate and run a CDK application
- * Install the AWS CDK: `npm install --rlobal aws-cdk`
- * To generate a new project, run: `cdk init app --language python`
+ * Install the AWS CDK: `npm install --global aws-cdk`
+ * To generate a new project, make and move into a new directory, run: `cdk init app --language python`
+ * Activate the virtual environment: `source .venv/bin/activate`
+ * Install/upgrade dependencies by running: `pip install --upgrade -r requirements.txt`
+ * To be able to run tests with pytest run `pip install --upgrade -r requirements-dev.txt`
  * The entry point for the app will be: `./app.py`
+
+## To work with the lastest code in this repo
+ * Make sure you have completed the initial setup steps
+ * Clone the repo `git clone https://github.com/Scottish-Tech-Army/eleos-serverless.git`
+ * Move into the cloned directory `cd eleos-serverless`
+ * Best to make a local development branch `git switch nameofyouruniquebranch`
+ * Move into the infrastructure directory, where the cdk code is kept `cd infrastructure`
+ * Create a virtual environment as .venv `python -m venv .venv`
+ * Activate the virtual environment with `source .venv/bin/activate`
+ * Install/upgrade dependencies by running: `pip install --upgrade -r requirements.txt`
+ * To be able to run tests with pytest also run `pip install --upgrade -r requirements-dev.txt`
+ * Continue to Development below
 
 ## Development
 
 To get started with development:
 
- * Activate the virtual environment: `source .venv/bin/activate`
- * Install/upgrade dependencies by running: `pip install --upgrade -r requirements.txt`
- * Edit infrastructure code in: `./eleos/eleos_stacl.py`
+ * Activate the virtual environment if it is not already active `source .venv/bin/activate`
+ * Edit infrastructure code in: `./eleos/eleos_stack.py`
+ * Synthesise the CloudFormation template with `cdk synth`
  * Check differences to be deployed by running: `cdk diff`
  * Deploy infrastructure changes by running `cdk deploy`
+
+## Tidy Up when you have finished
+ * Destroy the deployed app with `cdk destroy` in development to avoid extra costs
+ * Deactivate the vitual environment `deactivate`
 
 NB you will need to have AWS credentials to run `cdk deploy`.
 
--- infrastructure/tests/unit/test_eleos_stack.py
+++ infrastructure/tests/unit/test_eleos_stack.py
@@ -1,15 +1,129 @@
+from re import match
 import aws_cdk as core
-import aws_cdk.assertions as assertions
-
+from aws_cdk.assertions import Template, Match
 from eleos.eleos_stack import EleosStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in eleos/eleos_stack.py
-def test_sqs_queue_created():
+
+
+def test_dbname_matches():
     app = core.App()
     stack = EleosStack(app, "eleos")
-    template = assertions.Template.from_stack(stack)
+    template = Template.from_stack(stack)
+    
+    template.has_resource_properties("AWS::RDS::DBCluster", {
+        "DatabaseName": "postgres"
+        })
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+def test_dbuser_matches():
+    app = core.App()
+    stack = EleosStack(app, "eleos")
+    template = Template.from_stack(stack)
+
+    template.has_resource_properties("AWS::RDS::DBCluster", {
+        "MasterUsername": "odoo"
+        })
+
+def test_task_definition_mount_points():
+    app = core.App()
+    stack = EleosStack(app, "eleos")
+    template = Template.from_stack(stack)
+
+    template.has_resource_properties("AWS::ECS::TaskDefinition",
+        Match.object_like( {
+            "ContainerDefinitions": [
+                   {
+                    "Environment": [
+                        {
+                        "Name": "DB_PORT_5432_TCP_ADDR",
+                        "Value": {
+                            "Fn::GetAtt": [
+                                "postgres6BBC2FA4",
+                                "Endpoint.Address"
+                                ]
+                            }
+                        }
+                    ],
+                    "Essential": True,
+                    "Image": "odoo:latest",
+                    "LogConfiguration": {
+                        "LogDriver": "awslogs",
+                        "Options": {
+                            "awslogs-group": {
+                                "Ref": "odooFargateServiceTaskDefodooContainerLogGroupB787C48A"
+                                },
+                            "awslogs-stream-prefix": "odooFargateService",
+                            "awslogs-region": {
+                                "Ref": "AWS::Region"
+                                }
+                            }
+                        },
+                    "MountPoints": [
+                        {
+                        "ContainerPath": "/var/lib/odoo",
+                        "ReadOnly": False,
+                        "SourceVolume": "odooVolume"
+                        }
+                        ],
+                    "Name": "odooContainer",
+                    "PortMappings": [
+                       {
+                         "ContainerPort": 8069,           
+                         "Protocol": "tcp"
+                       }
+                     ],
+                     "Secrets": [
+                       {
+                         "Name": "POSTGRES_PASSWORD",
+                         "ValueFrom": {
+                           "Fn::Join": [
+                             "",
+                             [
+                               {
+                                 "Ref": "postgresSecretAttachmentE987A31D"
+                               },
+                               ":password::"
+                             ]
+                           ]
+                         }
+                       }
+                     ]
+                   }
+                 ],
+                 "Cpu": "512",
+                 "ExecutionRoleArn": {
+                   "Fn::GetAtt": [
+                     "odooFargateServiceTaskDefExecutionRoleC8AE90B9",
+                     "Arn"
+                   ]
+                 },
+                 "Family": "eleosodooFargateServiceTaskDefDD9DFCA8",
+                 "Memory": "2048",
+                 "NetworkMode": "awsvpc",
+                 "RequiresCompatibilities": [
+                   "FARGATE"
+                 ],
+                 "TaskRoleArn": {
+                   "Fn::GetAtt": [
+                     "odooFargateServiceTaskDefTaskRoleA7B2D8EF",
+                     "Arn"
+                   ]
+                 },
+                 "Volumes": [
+                   {
+                     "EfsVolumeConfiguration": {
+                       "AuthorizationConfig": {
+                         "AccessPointId": {
+                           "Ref": "odooEfsAccessPoint59A957B6"
+                         },
+                         "IAM": "ENABLED"
+                       },
+                       "FileSystemId": {
+                         "Ref": "odooEfsFileSystemD23A9885"
+                       },
+                       "TransitEncryption": "ENABLED"
+                     },
+                     "Name": "odooVolume"
+                   }
+                 ]
+             }
+        ))
