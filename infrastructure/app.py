62dfae13b94530f6e40df9dfe21989f6183911ea
4-3is4-me <timwornell@hotmail.com>
2023-08-25 09:21:03 +0100

    Tidy and painted black

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -6,37 +6,40 @@ from aws_cdk import App, Stack, Tags, Environment
 from eleos.eleos_stack import EleosStack
 
 # regions
-london = 'eu-west-2'
+london = "eu-west-2"
 # account numbers
-dev_test_acc = '113725134633'
-lms_stag_prod_acc = '131458236732'
-tampontaxi_stag_prod = '677304537696'
+dev_test_acc = "113725134633"
+lms_stag_prod_acc = "131458236732"
+tampontaxi_stag_prod = "677304537696"
 ##### other account numbers to be added here
 
 # Odoo versions
-odoo_16 = 'odoo:16'
-odoo_15 = 'odoo:15'
-odoo_14 = 'odoo:14'
+odoo_16 = "odoo:16"
+odoo_15 = "odoo:15"
+odoo_14 = "odoo:14"
 
 # Environments
-dev = 'dev'
-test = 'test'
-stage = 'stage'
-prod = 'prod'
+dev = "dev"
+test = "test"
+stage = "stage"
+prod = "prod"
 
 ### naming convention  = account - odoo version - environment
 
 app = App()
+
+
 def get_account():
     try:
         account = pull_stacks()[2]
         alias = pull_stacks()[3]
-        print(f'Working in account: {alias}')
+        print(f"Working in account: {alias}")
         return account
         sleep(2)
     except Exception as error:
-        print('Failed account check!')
-        print(f'{error}')
+        print("Failed account check!")
+        print(f"{error}")
+
 
 ## Below are the stacks defined in each account, add remove or edit here ##
 
@@ -45,60 +48,64 @@ def get_account():
 ########################################################################
 acc = get_account()
 if acc == dev_test_acc:
-    Eleos_dev_stack2 = EleosStack(app,
-        'dev-test-16-dev', # this is the construct_id used to call the stack
+    Eleos_dev_stack2 = EleosStack(
+        app,
+        "dev-test-16-dev",  # this is the construct_id used to call the stack
         env=Environment(account=dev_test_acc, region=london),
-        odoo_version = odoo_16,
-        environ = dev
-        )
+        odoo_version=odoo_16,
+        environ=dev,
+    )
 
-    Eleos_dev_stack = EleosStack(app,
-        'dev-test-14-dev',
+    Eleos_dev_stack = EleosStack(
+        app,
+        "dev-test-14-dev",
         env=Environment(account=dev_test_acc, region=london),
-        odoo_version = odoo_14,
-        environ = dev,
-        )
+        odoo_version=odoo_14,
+        environ=dev,
+    )
 
-    Tags.of(Eleos_dev_stack2).add('tag', 'ODOO 16 DEV')
-    Tags.of(Eleos_dev_stack).add('tag', 'DEV')
+    Tags.of(Eleos_dev_stack2).add("tag", "ODOO 16 DEV")
+    Tags.of(Eleos_dev_stack).add("tag", "DEV")
 
 ########################################################################
 #                       lms-stag-prod-acc                              #
 ########################################################################
 if acc == lms_stag_prod_acc:
-    Eleos_lms_stag_prod = EleosStack(app,
-        'lms-stag-prod-15-stag',
+    Eleos_lms_stag_prod = EleosStack(
+        app,
+        "lms-stag-prod-15-stag",
         env=Environment(account=lms_stag_prod_acc, region=london),
-        odoo_version = odoo_15,
-        environ = stage,
-        )
-    
-    Tags.of(Eleos_lms_stag_prod).add('tag', 'STAGE')
+        odoo_version=odoo_15,
+        environ=stage,
+    )
+
+    Tags.of(Eleos_lms_stag_prod).add("tag", "STAGE")
 
 ########################################################################
 #                      tampontaxi-stag-prod                            #
 ########################################################################
 if acc == tampontaxi_stag_prod:
-    Eleos_tampontaxi_stag_prod = EleosStack(app,
-        'tampontaxi-stag-prod-14-stag',
+    Eleos_tampontaxi_stag_prod = EleosStack(
+        app,
+        "tampontaxi-stag-prod-14-stag",
         env=Environment(account=tampontaxi_stag_prod, region=london),
-        odoo_version = odoo_14,
-        environ = stage,
-        )
-    
-    Tags.of(Eleos_tampontaxi_stag_prod).add('tag', 'STAG-SERVERLESS')
+        odoo_version=odoo_14,
+        environ=stage,
+    )
+
+    Tags.of(Eleos_tampontaxi_stag_prod).add("tag", "STAG-SERVERLESS")
 
 ########################################################################
 app.synth()
 
 
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+# If you don't specify 'env', this stack will be environment-agnostic.
+# Account/Region-dependent features and context lookups will not work,
+# but a single synthesized template can be deployed anywhere.
+# Uncomment the next line to specialize this stack for the AWS Account
+# and Region that are implied by the current CLI configuration.
+# env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+# Uncomment the next line if you know exactly what Account and Region you
+# want to deploy the stack to. */
+# env=cdk.Environment(account='123456789012', region='us-east-1'),
+# For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
--- infrastructure/list_stacks.py
+++ infrastructure/list_stacks.py
@@ -1,57 +1,71 @@
 #!/usr/bin/env python
 
-## Helpful to list stacks that are deployed in AWS for the current account and region, 
-## see their status and retrieve their outputs without having to go to the 
+## Helpful to list stacks that are deployed in AWS for the current account and region,
+## see their status and retrieve their outputs without having to go to the
 ## AWS Managament Console.
 ## Tim Wornell
 
 import boto3
 
+
 def pull_stacks():
-    client = boto3.client('cloudformation')
-    alias = boto3.client('iam').list_account_aliases()['AccountAliases'][0]
+    client = boto3.client("cloudformation")
+    alias = boto3.client("iam").list_account_aliases()["AccountAliases"][0]
     try:
         stacks = client.describe_stacks()
-        region = stacks['Stacks'][0]['StackId'][23:32]
-        account = stacks['Stacks'][0]['StackId'][33:45]
+        region = stacks["Stacks"][0]["StackId"][23:32]
+        account = stacks["Stacks"][0]["StackId"][33:45]
         return stacks, region, account, alias
     except Exception as error:
-        print(f'{error}\n')
+        print(f"{error}\n")
         exit()
 
+
 def print_output():
-    #print(stacks)
+    # print(stacks)
     ## create labels and set to display as green text
-    label = ['  status:  ', '    created:  ', 'Stack Name: ', 'Description: ', 'Updated: ', ' at: ',
-            'Outputs']
+    label = [
+        "  status:  ",
+        "    created:  ",
+        "Stack Name: ",
+        "Description: ",
+        "Updated: ",
+        " at: ",
+        "Outputs",
+    ]
     for i in range(len(label)):
-        label[i] = f'\x1b[0;32;40m{label[i]}\x1b[0m'
+        label[i] = f"\x1b[0;32;40m{label[i]}\x1b[0m"
 
     print()
-    print('\x1b[0;30;42m') #black on green!
-    print(' *************************************************')
-    print('               DEPLOYED STACKS                 ')
-    print(f'              Region: {region}                ')      
-    print(f'            AccountID: {account}            ')
-    print(f'            Account: {alias}            ')
-    print(' ************************************************** \x1b[0m')
+    print("\x1b[0;30;42m")  # black on green!
+    print(" *************************************************")
+    print("               DEPLOYED STACKS                 ")
+    print(f"              Region: {region}                ")
+    print(f"            AccountID: {account}            ")
+    print(f"            Account: {alias}            ")
+    print(" ************************************************** \x1b[0m")
     print()
 
     ## print list of stacks
-    for i in range(len(stacks['Stacks'])):
-        print(f' {i}: '+stacks['Stacks'][i]['StackName']
-                +label[0]+stacks['Stacks'][i]['StackStatus']
-                +label[1]+stacks['Stacks'][i]['CreationTime'].strftime('%d/%m/%Y'))
+    for i in range(len(stacks["Stacks"])):
+        print(
+            f" {i}: "
+            + stacks["Stacks"][i]["StackName"]
+            + label[0]
+            + stacks["Stacks"][i]["StackStatus"]
+            + label[1]
+            + stacks["Stacks"][i]["CreationTime"].strftime("%d/%m/%Y")
+        )
 
     ## take stack number as input and display select information
     while True:
         print()
         try:
-            choice = input('Enter a number for details or q to quit: ')
-            if choice == 'q':
+            choice = input("Enter a number for details or q to quit: ")
+            if choice == "q":
                 exit()
         except:
-            choice = 'q'
+            choice = "q"
             exit()
         try:
             item = int(choice)
@@ -59,31 +73,40 @@ def print_output():
             pass
         print()
         try:
-            print(label[2]+stacks['Stacks'][item]['StackName'])
+            print(label[2] + stacks["Stacks"][item]["StackName"])
             try:
-                print(label[3]+stacks['Stacks'][item]['Description'])
+                print(label[3] + stacks["Stacks"][item]["Description"])
             except:
                 pass
             try:
-                print(label[4]+stacks['Stacks'][item]['LastUpdatedTime'].strftime('%d/%m/%Y')
-                +label[5]+stacks['Stacks'][item]['LastUpdatedTime'].strftime('%H:%M:%S'))
+                print(
+                    label[4]
+                    + stacks["Stacks"][item]["LastUpdatedTime"].strftime("%d/%m/%Y")
+                    + label[5]
+                    + stacks["Stacks"][item]["LastUpdatedTime"].strftime("%H:%M:%S")
+                )
             except:
                 pass
             try:
                 print(label[6])
-                for i in range(len(stacks['Stacks'][item]['Outputs'])):
-                    print(' '+stacks['Stacks'][item]['Outputs'][i]['OutputKey']
-                    +':     '+stacks['Stacks'][item]['Outputs'][i]['OutputValue'])
+                for i in range(len(stacks["Stacks"][item]["Outputs"])):
+                    print(
+                        " "
+                        + stacks["Stacks"][item]["Outputs"][i]["OutputKey"]
+                        + ":     "
+                        + stacks["Stacks"][item]["Outputs"][i]["OutputValue"]
+                    )
             except:
-                print(' None')
+                print(" None")
                 pass
             print()
         except:
-            print('Try again')
+            print("Try again")
             pass
 
-if __name__ == '__main__':
-    
+
+if __name__ == "__main__":
+
     stacks, region, account, alias = pull_stacks()
-    #alias = boto3.client('iam').list_account_aliases()['AccountAliases'][0]
+    # alias = boto3.client('iam').list_account_aliases()['AccountAliases'][0]
     print_output()
